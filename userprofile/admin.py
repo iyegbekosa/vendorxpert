69d7ee079000d1c891e8adfe0129342442cce25f
21kosa <aikpitanyi.iyegbekosa@gmail.com>
2025-07-14 12:47:34 +0100

    vendor plan

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import UserProfile, VendorProfile
+from .models import UserProfile, VendorProfile, VendorPlan
 
 
 admin.site.register(UserProfile)
 admin.site.register(VendorProfile)
+admin.site.register(VendorPlan)
