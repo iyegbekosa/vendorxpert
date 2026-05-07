0f02ada440b2f8fabb607e30899cd6e68c0f1b67
Desmond Egya <egyadesmond@gmail.com>
2026-05-07 12:56:23 +0100

    feat: Change menu tag to simple_tag and update rendering to return safe HTML

--- store/templatetags/menu.py
+++ store/templatetags/menu.py
@@ -1,10 +1,14 @@
 from django import template
 from store.models import Category
+from django.template.loader import render_to_string
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
-@register.inclusion_tag('store/menu.html')
+
+@register.simple_tag
 def menu():
     categories = Category.objects.all()
 
-    return {'categories':categories}
+    html = render_to_string("store/menu.html", {"categories": categories})
+    return mark_safe(html)
