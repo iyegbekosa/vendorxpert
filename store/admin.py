9c86b384e26bbf844d25f81b7281dc6abf053ade
Desmond Egya <egyadesmond@gmail.com>
2026-05-07 13:36:42 +0100

    feat: Register Category model with custom admin class for improved display and slug management

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Category, Product, Review, Order, OrderItem
 
-admin.site.register(Category)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ("title", "slug")
+	prepopulated_fields = {"slug": ("title",)}
+
+
 admin.site.register(Product)
 admin.site.register(Review)
 admin.site.register(Order)
