eb83d7b27c66f73b38848a5e2df38af6bb917b7f
Desmond Egya <egyadesmond@gmail.com>
2026-05-07 12:59:59 +0100

    feat: Update Pipfile dependencies and patch Django template context copy method

--- vendorxpert/settings.py
+++ vendorxpert/settings.py
@@ -195,6 +195,21 @@ REST_FRAMEWORK = {
     )
 }
 
+
+def _patch_django_template_context_copy():
+    from django.template.context import BaseContext
+
+    def _safe_copy(self):
+        duplicate = self.__class__.__new__(self.__class__)
+        duplicate.__dict__.update(self.__dict__)
+        duplicate.dicts = self.dicts[:]
+        return duplicate
+
+    BaseContext.__copy__ = _safe_copy
+
+
+_patch_django_template_context_copy()
+
 # JWT Settings
 from datetime import timedelta
 
